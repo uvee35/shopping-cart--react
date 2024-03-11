be77ed1fcb7ce82a5c624e757dc5f6e3d1b7e904
Victor Marques <151088688+uvee35@users.noreply.github.com>
2024-03-11 21:36:24 +0000

    Refactor App component to use useState and add product filtering

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
-import {Products} from "./components/Products.jsx"
-import { products } from "./mocks/product.json"
-function App() {
-  return(
+import { useState } from "react";
+import { product as initialProducts } from "./mocks/products.json";
+import { Products } from "./components/Products.jsx";
 
-    <Products products= {products}/>
-  
-  )
+function App() {
+  const [products] = useState(initialProducts)
+  const[filters,setFilter] = useState({
+    category: 'all',
+    minPrice: 0,
+})
+
+  const filterProducts = (products) => {
+    return products.filter(product => {
+      return(
+        product.price >= filters.minPrice &&
+        (filters.category === 'all' || product.category === filters.category)
+      )
+    })
+  }
+
+  return (
+    <div>
+      <Products products={products} />
+    </div>
+  );
 }
 
-export default App
+export default App;
