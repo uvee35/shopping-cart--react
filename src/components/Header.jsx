5888e76da06f06646bc5222e4a179fadfe1d6f99
Victor Marques <151088688+uvee35@users.noreply.github.com>
2024-03-11 21:55:10 +0000

    Refactor Header component to include Filters component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,10 @@
-import {Filters} from './Filters.jsx';
+import { Filters } from './Filters.jsx'
+
+export function Header () {
+  return (
+    <header>
+      <h1>React Shop 🛒</h1>
+      <Filters />
+    </header>
+  )
+}
