68d6ce39f289b8fc91fe7864652baf497e0e124f
Victor Marques <151088688+uvee35@users.noreply.github.com>
2024-03-11 21:54:16 +0000

    Fix file import and update CSS file names

---
+++ src/components/Filters.jsx
@@ -0,0 +1,54 @@
+import { useId } from 'react'
+import { useFilters } from '../hooks/useFilters.js'
+import './Filters.css'
+
+export function Filters () {
+  const { filters, setFilters } = useFilters()
+
+  const minPriceFilterId = useId()
+  const categoryFilterId = useId()
+
+  const handleChangeMinPrice = (event) => {
+    setFilters(prevState => ({
+      ...prevState,
+      minPrice: event.target.value
+    }))
+  }
+
+  const handleChangeCategory = (event) => {
+  
+    setFilters(prevState => ({
+      ...prevState,
+      category: event.target.value
+    }))
+  }
+
+  return (
+    <section className='filters'>
+
+      <div>
+        <label htmlFor={minPriceFilterId}>Price from</label>
+        <input
+          type='range'
+          id={minPriceFilterId}
+          min='0'
+          max='1000'
+          onChange={handleChangeMinPrice}
+          value={filters.minPrice}
+        />
+        <span>${filters.minPrice}</span>
+      </div>
+
+      <div>
+        <label htmlFor={categoryFilterId}> Categories</label>
+        <select id={categoryFilterId} onChange={handleChangeCategory}>
+          <option value='all'>All</option>
+          <option value='laptops'>Laptops</option>
+          <option value='smartphones'>Cellphones</option>
+        </select>
+      </div>
+
+    </section>
+
+  )
+}
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,11 +1,28 @@
-import "./product.css";
-import { AddToCartIcon } from "./icons";
+import './Products.css'
+import { AddToCartIcon } from './Icons.jsx'
 
-export default function Products({ Products }) {
+export default function Products({ products }) {
     return (
         <main className="products">
             <ul>
                 {products.map((product) => (
+                    <li key={product.id}>
+                        <img src={product.thumbnail} 
+                        alt={product.Title} />
+                        <div>
+                            <strong>{product.title}</strong> -$ {product.price}
+                        </div>
+                        <div>
+                            <button>    
+                                <AddToCartIcon />
+                            </button>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </main>
+    );
+}
                     <li key={product.id}>
                         <img src={product.thumbnail} 
                         alt={product.Title} />
