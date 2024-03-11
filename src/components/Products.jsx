a9a405ac7af181285f84174fa27358df7f1f8173
Victor Marques <151088688+uvee35@users.noreply.github.com>
2024-03-11 22:04:15 +0000

    Refactor Products component to add cart functionality

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,43 +1,51 @@
 import './Products.css'
-import { AddToCartIcon } from './Icons.jsx'
+import { AddToCartIcon, RemoveFromCartIcon } from './Icons.jsx'
+import { useCart } from '../hooks/useCart.js'
 
-export default function Products({ products }) {
-    return (
-        <main className="products">
-            <ul>
-                {products.map((product) => (
-                    <li key={product.id}>
-                        <img src={product.thumbnail} 
-                        alt={product.Title} />
-                        <div>
-                            <strong>{product.title}</strong> -$ {product.price}
-                        </div>
-                        <div>
-                            <button>    
-                                <AddToCartIcon />
-                            </button>
-                        </div>
-                    </li>
-                ))}
-            </ul>
-        </main>
-    );
-}
-                    <li key={product.id}>
-                        <img src={product.thumbnail} 
-                        alt={product.Title} />
-                        <div>
-                            <strong>{product.title}</strong> -$ {product.price}
-                        </div>
-                        <div>
-                            <button>    
-                                <AddToCartIcon />
-                                </button>
-                        </div>
-                    </li>
-                ))}
-            </ul>
-        </main>
-    );
-}
+export function Products ({ products }) {
+  const { addToCart, removeFromCart, cart } = useCart()
 
+  const checkProductInCart = product => {
+    return cart.some(item => item.id === product.id)
+  }
+
+  return (
+    <main className='products'>
+      <ul>
+        {products.slice(0, 10).map(product => {
+          const isProductInCart = checkProductInCart(product)
+
+          return (
+            <li key={product.id}>
+              <img
+                src={product.thumbnail}
+                alt={product.title}
+              />
+              <div>
+                <strong>{product.title}</strong> - ${product.price}
+              </div>
+              <div>
+                <button
+                  style={{ backgroundColor: isProductInCart ? 'red' : '#09f' }} onClick={() => {
+                    isProductInCart
+                      ? removeFromCart(product)
+                      : addToCart(product)
+                  }}
+                >
+                  {
+                    isProductInCart
+                      ? <RemoveFromCartIcon />
+                      : <AddToCartIcon />
+                  }
+                </button>
+              </div>
+            </li>
+          )
+        })}
+      </ul>
+    </main>
+    )
+
+        
+
+}
